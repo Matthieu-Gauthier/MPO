71537f6269adf4310daaa2ede2b013d36fd45b89
Matthieu <matthieu.gauthier@bewave.io>
2022-05-30 14:10:20 -0400

    some fix lint + test variables theme sass

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <v-app>
-     <v-app-bar
-        color="teal-darken-4"
-        image="https://picsum.photos/1920/1080?random"
-     />
-    <v-main class="app">
-       <v-btn color="warning"
-              to="/">
-          Home
-       </v-btn>
-       <v-btn color="primary"
-              to="/about">
-          About
-       </v-btn>
+   <v-app>
+      <v-app-bar color="teal-darken-4"
+                 image="https://picsum.photos/1920/1080?random" />
+      <v-main class="app">
+         <v-btn color="warning"
+                to="/">
+            Home
+         </v-btn>
+         <v-btn color="primary"
+                to="/about">
+            About
+         </v-btn>
 
-       <router-view/>
-    </v-main>
-  </v-app>
+         <v-btn color="secondary">
+            secondary
+         </v-btn>
+         <v-btn color="accent">
+            accent
+         </v-btn>
+         <v-btn color="error">
+            error
+         </v-btn>
+         <v-btn color="warning">
+            warning
+         </v-btn>
+         <v-btn color="info">
+            info
+         </v-btn>
+         <v-btn color="success">
+            success
+         </v-btn>
+         <router-view />
+      </v-main>
+   </v-app>
 </template>
-
-<script setup>
-import appBar from '@/components/AppBar.vue'
-</script>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+   <v-container>
+      <v-btn absolute
+             append-icon="ded">
+         CLick
+      </v-btn>
+      <v-btn> {{ a }}</v-btn>
+   </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const a = 'abg';
+const b = ref(2);
+defineExpose({
+   a,
+   b
+});
+</script>
