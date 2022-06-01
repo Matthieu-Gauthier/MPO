443c86f6f9ff8003bec94cebb7aebf93f744a123
Matthieu <matthieu.gauthier@bewave.io>
2022-06-01 19:16:04 -0400

    add appbar first commit

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -1,6 +1,33 @@
 <template>
-   <div>Mon App Bar</div>
+   <v-toolbar absolute
+              color="var(--primary-dark-color)"
+              class="rounded-pill text-white"
+              style="top: 3em;left: 50%; transform: translateX(-50%)">
+      <v-btn v-for="link in routes"
+             :key="link.name" :to="link.path"
+             class="rounded-pill">
+         {{ link.name }}
+      </v-btn>
+
+      <v-btn icon
+             class="ml-16">
+         <v-avatar image="https://picsum.photos/40/40"
+                   large />
+      </v-btn>
+   </v-toolbar>
 </template>
 
 <script setup>
+const routes = [{
+   path: '/',
+   name: 'home',
+},
+{
+   name: 'login',
+   path: '/login',
+},
+{
+   name:'about',
+   path:'/about',
+}];
 </script>
