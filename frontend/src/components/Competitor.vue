1b1af5d5f5623be5743bdf6e5097faf55875e906
Matthieu Gauthier <matthieu.gauthier@abbeal.com>
2022-06-01 21:39:38 -0400

    create fight vue and competitor component

---
+++ frontend/src/components/Competitor.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+      <v-img :src="item.banner"
+             large />
+      <v-img :src="item.avatar" />
+   </div>
+</template>
+
+<script setup>
+import { useGet } from 'feathers-pinia';
+import { useCompetitors } from '@/services/competitors';
+const props = defineProps({
+   id: { type: String }
+});
+const competitors = useCompetitors();
+const { item } =  useGet({ model: competitors.Model, id: props.id });
+</script>
--- frontend/src/views/Fight.vue
+++ frontend/src/views/Fight.vue
@@ -1,17 +1,19 @@
 <template>
    <div>
-      {{ items }}
       <h1>{{ id }}</h1>
+      <competitor v-if="item"
+                  :id="item.away_competitor_id" />
    </div>
 </template>
 
 <script setup>
-import { useFind } from 'feathers-pinia';
+import Competitor from '@/components/Competitor.vue';
+import { useGet } from 'feathers-pinia';
 import { useFights } from '@/services/fights';
 const props = defineProps({
    id: { type: String }
 });
 
 const fights = useFights();
-const { items } =  useFind({ model: fights.Model, params: { query: { id: props.id } } });
+const { item } =  useGet({ model: fights.Model, id: props.id });
 </script>
